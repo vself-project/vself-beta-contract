0198846ac5101eab0e702c0e6a448dcf861c5cf7
SergantChe <sergei.alex.kozlov@gmail.com>
2022-03-02 01:58:07 +0300

    Add js script to testnet contract deployment

---
+++ scripts/deploy.testnet.js
@@ -0,0 +1,17 @@
+const sh = require('shelljs');
+const { CONTRACT_NAME, MASTER_ACCOUNT} = process.env;
+const DELETE_BEFORE_DEPLOY = process.env.DELETE_BEFORE_DEPLOY === "true";
+
+// Recreate account
+if (DELETE_BEFORE_DEPLOY) {
+  sh.exec(`near delete ${CONTRACT_NAME} ${MASTER_ACCOUNT}`);
+}
+sh.exec(`near create-account ${CONTRACT_NAME} --masterAccount=${MASTER_ACCOUNT} --initial-balance 100`);
+
+// Deploy contract
+sh.exec(`near deploy --wasmFile out/main.wasm --accountId ${CONTRACT_NAME}`);
+sh.exec(`near call ${CONTRACT_NAME} new --accountId ${CONTRACT_NAME}`);
+sh.exec(`near view ${CONTRACT_NAME} is_active`);
+
+// Copy credentials for later deploy
+sh.exec(`cp ~/.near-credentials/testnet/${CONTRACT_NAME}.json ./creds`);
